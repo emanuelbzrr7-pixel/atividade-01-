e84e9258014bcd136d6f123c41b7d700c9bcf384
emanuelbzrr7-pixel <emanuelbzrr7@gmail.com>
2025-08-20 10:50:39 -0300

    Create atividade01.java

---
+++ atividade01.java
@@ -0,0 +1,25 @@
+public class Atividade01{
+    public static void main(String[] args) {
+// Notas fixas para exemplos
+        double[] notas = {10.0, 9.0, 7.6, 0, 8.0, 8.0, 7.9};
+//Cálculo das médias bimestrais
+        double b1 = (notas[0] + notas[1]) / 2;
+        double b2 = (notas[2] + notas[3]) / 2;
+        double b3 = (notas[4] + notas[5]) / 2;
+        double b4 = (notas[6] + notas[5]) / 2;
+// Cálculo das médias semestrais
+        double s1 = (b1 + b2) / 2;
+        double s2 = (b3 + b4) / 2;
+// Cálculo da média final
+        double mediaFinal = (s1 + s2) / 2;
+// Saida formatada
+        System.out.println("\nPráticas\n");
+        System.out.printf("1ºBimestre:%.1f\n", b1);
+        System.out.printf("2ºBimestre:%1f\n", b2);
+        System.out.printf("1ºSemestre:%.1f\n", s1);
+        System.out.println("----------------------");
+        System.out.printf("3ºBimestre:%.1f\n", b3);
+        System.out.printf("4ºBimester:%.1f\n", b4);
+        System.out.printf("2ºSenestre:%.1f\n", s2);
+        System.out.println("----------------------");
+        System.out.printf("Média Final: %.1f\n", mediaFinal);
